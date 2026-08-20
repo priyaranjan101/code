7a7a2fa5cc9441e00d40449877deff354d6eca34
priyaranjan101 <pattanayak.priyaranjan2006@gmail.com>
2026-08-20 17:16:46 +0530

    Time: 0 ms (100%), Space: 10.1 MB (46.94%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.c
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.c
@@ -2,23 +2,21 @@
  * Note: The returned array must be malloced, assume caller calls free().
  */
 int* twoSum(int* numbers, int numbersSize, int target, int* returnSize) {
-    int i = 0, j = numbersSize - 1,sum;
-    int* result = (int*)malloc(2 * sizeof(int));
-    *returnSize = 2;
-    while (i < j) {
-        sum = numbers[i] + numbers[j];
-        if (sum == target) {
-            result[0] = i+1;
-            result[1] = j+1;
-            return result;
-        }
-        if (sum < target) {
-            i++;
-        } 
-        else {
+    int i = 0, j = numbersSize - 1, m;
+    int* ans = malloc(2 * sizeof(int));
+    *returnSize=2;
+    
+    while (i <= j) {
+        m = numbers[i] + numbers[j];
+        if (m == target){
+        ans[0]=i+1;
+        ans[1]=j+1;
+            return ans;
+    }
+        if (m > target)
             j--;
-        }
+        else
+            i++;
     }
     return NULL;
-    
 }
