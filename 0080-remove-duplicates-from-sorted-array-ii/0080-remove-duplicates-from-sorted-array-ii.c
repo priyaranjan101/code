d624cfc86676b9f889dd5a2fe9526817c33d3565
priyaranjan101 <pattanayak.priyaranjan2006@gmail.com>
2026-08-20 19:42:32 +0530

    Time: 26 ms (77.76%), Space: 15.1 MB (54.75%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.c
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.c
@@ -2,13 +2,13 @@ int removeDuplicates(int* nums, int numsSize) {
     int i = 0, j = 2, k = 2;
     if(numsSize<=2)
     return numsSize;
-    
+
     while (j < numsSize) {
         if (nums[j] == nums[i])
             j++;
         else {
-            nums[k] = nums[j];
-            k++;
+            nums[k++] = nums[j];
+           
             i++;
             j++;
         }
